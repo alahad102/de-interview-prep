d3b01221803d5a4094c707253628168bc3345de7
alahad102 <alahad102@gmail.com>
2026-07-24 15:23:08 -0500

    q6 solved without window function

--- sql/window_function/window_function_practice.sql
+++ sql/window_function/window_function_practice.sql
@@ -83,10 +83,21 @@ WHERE salary_tier <= 2 AND tie_breaker = 1;
 
 -- Q6: For every employee, add a column showing the highest salary
 -- in their department -- repeated on every row, not just the top one.
+
 SELECT
     employee_id, first_name, last_name, department_id, salary,
     MAX(salary) OVER (PARTITION BY department_id) AS highest_salary
 FROM employees;
+
+SELECT
+    e.employee_id, e.first_name, e.last_name, e.department_id, e.salary,
+    d.highest_salary
+FROM employees e
+JOIN (
+    SELECT department_id, MAX(salary) AS highest_salary
+    FROM employees
+    GROUP BY department_id
+) d ON e.department_id = d.department_id;
 
 -- Q7: For every employee, add a column showing the LOWEST salary
 -- in their department, repeated on every row.
