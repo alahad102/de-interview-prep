3a76386834f78030f2799d24287275ded3e59252
alahad102 <alahad102@gmail.com>
2026-08-11 06:00:33 -0500

    exploring different print function

--- python/hello.py
+++ python/hello.py
@@ -1,1 +1,21 @@
-print('   hello   ')
+print('hello')
+
+# writing random comment
+
+print("abc \nde")
+
+print('HI \\"ahad\\"')
+
+# escape
+# using backslash to tell skip a character
+print("path: C:\\users\\ahad")
+
+print("msg1 \n")
+print("msg2")                                                               
+
+# print exercise
+
+print("""My learning path:
+      \t-python basic
+      \t-Data Engineering
+      \t-AI""")
