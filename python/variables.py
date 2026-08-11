c9e1c91e58f36c70f4b833c9da70b966bc5e354f
alahad102 <alahad102@gmail.com>
2026-08-11 06:19:20 -0500

    exploring varibles and f string

---
+++ python/variables.py
@@ -0,0 +1,18 @@
+
+
+x = 'Ahad'
+print("hello, this is",x)
+print(x, "is learning python")
+print(x, "is practicing daily for that")
+
+print(f"Hello, this is {x}")
+print(f"{x} is learning python")
+print(f"{x} is practicing daily for that")
+
+
+x = 'datawith'
+y = 'ahad'
+z = '.com'
+print(f"info@{x}{y}{z}")
+print(f"support@{x}{y}{z}")
+print(f"www.{x}{y}{z}")
