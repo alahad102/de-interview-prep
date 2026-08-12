33e500cfbf20bf33e8fcee84814d82a5b1c235b1
alahad102 <alahad102@gmail.com>
2026-08-11 19:10:01 -0500

    exploring datatypes and function vs method

---
+++ python/data_type.py
@@ -0,0 +1,14 @@
+# data types
+a = 10 #int
+b = 3.1416 #float
+c = "Ahad" #string
+d = 'khan'  #string
+e = "12345" # string
+f = True #bool and case sensitive
+g = False #bool
+h = None  #no value
+i = "" #string-blank
+j = " " #string - empty space
+
+print(d.upper())
+
