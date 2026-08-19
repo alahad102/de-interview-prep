39e71dd37eb129207d9177e8d93e6b7abe554ed3
alahad102 <alahad102@gmail.com>
2026-08-19 12:46:59 -0500

    exploring diff operation of numbers

---
+++ python/numbers.py
@@ -0,0 +1,31 @@
+a = 1
+b = 5.6
+c = 2 + 3j
+# print(type(a))
+# print(type(b))
+# print(type(c))
+
+x = "24"
+# print(type(x))
+x = int(x)
+# print(type(x))
+
+# print(abs(2 - 10))
+import math
+# print(math.floor(2.5))
+
+#truc
+x = 3.14165
+# print(math.trunc(x))
+
+import random
+# print(random.randint(1,6))
+# print(random.random())
+
+#validation
+
+x = 7.0000
+# print(x.is_integer())
+x = 70.56
+print(isinstance(x, int))
+print(isinstance(x, float))
