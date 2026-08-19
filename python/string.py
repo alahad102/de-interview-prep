f48c19e251fbea5a8b5db05612d562ac36211c5a
alahad102 <alahad102@gmail.com>
2026-08-19 11:10:15 -0500

    more exploration on string in python

--- python/string.py
+++ python/string.py
@@ -85,4 +85,78 @@ stamp = "2026-09-20 14:30"
 # print(stamp.split(" "))
 
 csv_file = "1234,Max,USA,76706,M"
-# print(csv_file.split(","))
+#print(csv_file.split(","))
+
+
+
+#
+# print("ha" * 3)
+# print("*" * 30)
+# print("*" * 30)
+
+
+# extract specific part of string
+
+text = "ahadkhan"
+# print(text[-4])
+
+# slicing
+
+# print(text[-4:])
+# print(text[1:])
+
+# print(text[0:7:2])
+
+# string repition
+# indexing(single character) and slicing(multiple character)
+
+#cleaning
+#clean whitespaces
+#clean cases
+
+#lstrip, strip, rstrip
+
+text = " Engineering".lstrip()
+#print(text)
+
+text = "Engineering ".rstrip()
+# print(text)
+
+text = "                Engineering ".strip()
+#print(text)
+
+text = "Data Engineering".strip()
+# print(text)
+
+
+text = "###Abc###".strip("#")
+#print(text)
+
+text = " i am ahad  "
+# print(len(text.strip()))
+
+# case conversion upper , lower
+
+text = "data ENGINEERING"
+# print(text)
+# print(text.upper())
+# print(text.lower())
+
+search = "Email ".lower().strip()
+data = "eMaIl ".lower().strip()
+# print(search == data)
+
+#challenge
+
+text = "968-Maria, ( D@t@ Engineer );; 27y   "
+
+# String searching
+
+text = "2026-feb-11"
+# print(text.startswith('2026'))
+# print(text.endswith('10'))
+# print(text.find('feb'))
+
+phone = "+49-176-12345"
+print(phone.startswith("+49"))
+print("-" in phone)
