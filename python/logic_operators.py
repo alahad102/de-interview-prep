f6769f90a8242148649ed597adc445d552cd4c8d
alahad102 <alahad102@gmail.com>
2026-08-19 21:42:53 -0500

    more exploration

---
+++ python/logic_operators.py
@@ -0,0 +1,11 @@
+# conditional statement
+
+# any or all 
+
+email = ""
+phone = "015465"
+username = ""
+
+# print(all([email, phone,username]))
+# print(any([email, phone,username]))
+print(3>2)
---
+++ python/problem1.py
@@ -0,0 +1,12 @@
+# Even or Odd
+# Ask the user for a number and print whether it is even or odd.
+
+# Input: 17
+# Output: Odd
+
+x = int(input("Enter a number: "))
+
+if x % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
