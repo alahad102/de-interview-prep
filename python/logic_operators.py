22d31b18982d0f7f047ff7735a465cb8505a7be4
alahad102 <alahad102@gmail.com>
2026-08-21 12:53:18 -0500

    exploring different opearator and solved 5 mini problem

--- python/logic_operators.py
+++ python/logic_operators.py
@@ -8,4 +8,48 @@ username = ""
 
 # print(all([email, phone,username]))
 # print(any([email, phone,username]))
-print(3>2)
+# print(3>2)
+
+# print(3>2 and 2>3)
+
+text = "data eng"
+
+# print( 'ata' in text)
+# print( 'ahad' in ['aha', 'ahad'])
+
+domain = "gmail.com"
+banned_domains = ["spam.com", "fake.org", "bot.net"]
+# print(domain in banned_domains)
+
+a = [1,2,3]
+b = [1,2,3]
+# print(a is b)
+
+# problem 1
+
+# x = input("enter user name: ")
+# y = input("enter user age: ")
+# print(x != "" and int(y) >= 18)
+
+#problem 2
+
+# password = input("enter password: ")
+# print(len(password) >= 8 and (" " not in password))
+
+# problem 3
+
+# email = input("enter email address: ")
+# print(email != "" and '@' in email and email.endswith(".com"))
+
+# problem 4
+
+# user_name = input("enter a user name: ")
+# print(isinstance(user_name, str) and user_name is not None and len(user_name) > 5)
+
+# problem 5
+
+is_admin = True
+is_moderator  = True
+is_banned = True
+is_verified = False
+print((is_admin or is_moderator) and (not is_banned or is_verified))
