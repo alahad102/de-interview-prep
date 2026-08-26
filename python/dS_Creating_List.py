d482444d428ddd007c9eb3204d99a13944321d96
alahad102 <alahad102@gmail.com>
2026-08-26 07:38:25 -0500

    explore the firs tds list and how to create diffrent list in different way

---
+++ python/dS_Creating_List.py
@@ -0,0 +1,36 @@
+# create list
+
+empty = []
+letters = ['a','b','c']
+numbers = [1,2,3]
+mixed = [1 , 'a', True, None]
+# print(letters)
+# print(type(letters))
+# print(numbers)
+# print(type(numbers))
+# print(mixed)
+# print(type(mixed))
+
+empty = list()
+# print(empty)
+# print(type(empty))
+
+letters = list('Python')
+# print(letters)
+
+numbers = list(range(5))
+# print(numbers)
+
+#nested list
+
+matrix = [['a','b','c'],
+          ['d', 'e', 'f']]
+print(matrix)
+print(type(matrix))
+
+mixed_matrix = [['a','b'],
+                [1, 2, 3],
+                [True]]
+print(mixed_matrix)
+print(type(mixed_matrix))
+
