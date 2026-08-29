bfb951cc95e5b700e869777db776155568a1c231
alahad102 <alahad102@gmail.com>
2026-08-29 18:27:32 -0500

    learn how to update value in list

---
+++ python/ds_update_list.py
@@ -0,0 +1,28 @@
+# updating list
+
+numbers = [1,2,3,4]
+numbers[0] = 10
+print(numbers)
+
+letters = ['a','b','c']
+letters[0] = 'x'
+print(letters)
+
+matrix = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+print(matrix)
+matrix[-1] = [10,11,12]
+print(matrix)
+
+matrix = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+
+matrix[0][0] = 100
+matrix[1][1] = 500
+print(matrix)
