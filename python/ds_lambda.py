b966a6a41bc996264f8dacb6894f0734cc44ba7f
alahad102 <alahad102@gmail.com>
2026-09-01 09:57:06 -0500

    learned lambda function with map and filter and many use cases

---
+++ python/ds_lambda.py
@@ -0,0 +1,38 @@
+# lambda
+# own custom logic
+# anonymous function
+
+multiply = lambda x: x * 2
+print(multiply(6))
+
+addition = lambda x,y: x + y
+print(addition(5,2))
+
+check = lambda i: i in "python"
+print(check('x'))
+
+# lambda + map
+
+prices = ['$12.50', '$9.99','$100.00']
+
+
+x = lambda p: float(p.replace('$', ''))
+
+print(list(map(x, prices)))
+
+# lambda + filter
+
+prices = [120, 30,300,80]
+
+print(list(filter(lambda p: p >= 100, prices)))
+
+students = [
+    ['Maria', 85],
+    ['Kumar', 90],
+    ['Max', 60]
+]
+print(list(filter(lambda row: row[1] > 70, students)))
+
+print(list(filter(lambda row: row[0].startswith('M'), students)))
+
+students[0][1] > 70
